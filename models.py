717ebfe18f08636b6b15c41828f71e94bba5a4a4
Stephan-kashkarov <kashkarovwork@gmail.com>
2019-05-21 11:57:40 +1000

    reformatted file

--- models.py
+++ models.py
@@ -5,19 +5,8 @@ import numpy as np
 import pandas as pd
 import tensorflow as tf
 
-class Facial_Detection_Network(keras.models.Model):
-    def __init__(self):
-        super().__init__()
-        self.region_prop_network = None
-        self.facial_classification_network = None
-        self.region_extraction_network = None
-
-    def call(self, frame):
-        region_proposals = self.region_prop_network.predict(frame)
-        for index, region in self.region_extraction_network(frame, region_proposals):
-            if self.facial_classification_network.predict(region, batch_size=1)[0]:
-                regions.pop(index)
-        return regions
+def region_extraction(frame, regions):
+    pass
 
 
 class Region_Propositional_Network(keras.models.Model):
@@ -27,18 +16,28 @@ class Region_Propositional_Network(keras.models.Model):
     def call(self, frame):
         pass
 
-class Region_Extraction_Network(keras.models.Model):
-    def __init__(self):
-        super().__init__()
-
-    def call(self, frame, region):
-        pass
-                
-class facial_Classification_Network(keras.models.Model):
+            
+class Facial_Classification_Network(keras.models.Model):
     def __init__(self):
         super().__init__()
 
     def call(self, face):
         pass
+
+
+class Facial_Detection_Network(keras.models.Model):
+    def __init__(self):
+        super().__init__()
+        self.region_prop_network = Region_Propositional_Network()
+        self.facial_classification_network = Facial_Classification_Network()
+
+    def call(self, frame):
+        region_proposals = self.region_prop_network.predict(frame)
+        for index, region in region_extraction(frame, region_proposals):
+            if self.facial_classification_network.predict(region, batch_size=1)[0]:
+                regions.pop(index)
+        return regions
+
+
 
         
